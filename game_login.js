5afaf26aaf5077bb9ed358b741b10733dd792806
balajagruth <85967411+balajagruth@users.noreply.github.com>
2022-06-27 17:57:47 -0500

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,1 @@
+function addUser() { player1_name = document.getElementById("player1_name_input").value; player2_name = document.getElementById("player2_name_input").value; localStorage.setItem("User1",player1_name); localStorage.setItem("User2",player2_name); window.location = "game_page.html"; }
